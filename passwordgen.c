09e6640131ad56d1ad7cc13e23fdc3d0df33f948
Christopher <doege.chris@yahoo.com>
2015-09-25 09:56:12 -0500

    Fixed certain documentation errors
    Updated the documentation to reflect what I've done and explain some of my magic. Fixed certain variable names.
    
    Still needs further documentation to be human readable.

--- passwordgen.c
+++ passwordgen.c
@@ -8,7 +8,7 @@ void printPasswords(char numbers[], int x);
 int checkIllegalChars(char numbers[], int x);
 
 //determines the letters for the phone based on num
-char *determineLetters(char num) {
+char *determineLetters(char num) { //Uses the user's input to decide upon the available ranges
     switch (num) {
         case '2':
           return "abcdefghiklmnopqrstuvwxyz";
@@ -19,44 +19,37 @@ char *determineLetters(char num) {
         case '5':
           return "!@#$^&*()";
         case '6':
-          return "abcdefghiklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ0123456789!@#$^&*()";
-        case '7':
-          return "";
-        case '8':
-          return "";
-        case '9':
-          return "";
+          return "abcdefghiklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ0123456789!@#$^&*()"; //The number of the beast
         default:
             return NULL;
     }
 }
 //recurisve function that assigns values based on the number and position
 //was challenging to think of how I'd recurse this
-void printPasswords(char numbers[], int x) {
-    char newnum = numbers[x];
+void printPasswords(char numbers[], int position) { //Prints the total range of passwords from user's given range
+    char newnum = numbers[position];
     if (newnum == 0) {
         printf("\t%s\n", numbers);
     } else {
         char *letters = determineLetters(newnum);
         if (letters==NULL) {
-            numbers[x] = '*';
-            printPasswords(numbers, x + 1);
+            numbers[position] = '*';
+            printPasswords(numbers, position + 1);
         } else {
             for(;*letters!=0; letters++) {
-                numbers[x] = *letters;
-                printPasswords(numbers, x + 1);
+                numbers[position] = *letters;
+                printPasswords(numbers, position + 1);
             }
         }
-        numbers[x] = newnum;
+        numbers[position] = newnum;
     }
 }
 //wrapper function to both hide how things work and allow for the driver to call
-void listPasswords(char *numbers) {
-  int i;
-  int x = strlen(numbers);
+void listPasswords(char *numbers) { //Wrapper and error checking for the control 
+  int bFlag;
   printf("Passwords for %s are: \n", numbers);
-  i = checkIllegalChars(numbers, strlen(numbers));
-  if(i==0)
+  bFlag = checkIllegalChars(numbers, strlen(numbers)); //Checks user input to make sure it isn't ouside of bounds
+  if(bFlag==0)
     printPasswords(numbers, 0);
 }
 
@@ -68,10 +61,10 @@ int checkIllegalChars(char numbers[], int x)
   {
     if(numbers[i] == '0' || numbers[i]== '1')
     {
-      printf("\tNone, %s contains a digit 1 || 0.\n", numbers);
+      printf("\tNone, %s contains a digit 1 || 0.\n", numbers); // Exit characters
       return 1;
     }
-    if(!isdigit(numbers[i]))
+    if(!isdigit(numbers[i]) || (int)numbers[i]>6 || (int)numbers[i]<0) //Outside of datset and boundry cases
     {
       printf("\tNone, %s contains at least one alphabetic character.\n", numbers);
       return 1;
